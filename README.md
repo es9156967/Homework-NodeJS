22d07a388570d56190a4459d50826a88d32afdec
Eduardo Sanchez <edsanchez629@gmail.com>
2020-03-01 14:08:26 -0800

    updating index.js

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Homework-NodeJS
-Node JS
+test
--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
-var pdf = require('html-pdf');
  
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptuser() {
+function promptUser() {
   inquirer.prompt({
   message: "Enter your GitHub username:",
   name: "username"
   })
+  .then(function({ username }) {
+    const queryURL = `https://api.github.com/users/${username}`;
+    console.log (queryURL);
+
+    axios 
+  .get(queryURL)
+  .then(function(res){
+    const gitStats = {
+    "name": res.data.name,
+     "bio":res.data.bio,
+    "blog":res.data.blog,
+    "public_repos":res.data.public_repos,
+    "followers":res.data.followers,
+    "page_Url":res.data.html_url,
+    "location":res.data.location,
+    }
+    console.log(gitStats);
+
+  })
+
+});
+
 };
+
+promptUser();
+
