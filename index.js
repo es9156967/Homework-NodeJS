801283769bf2abc9036c72fa88100f3e86639f84
Eduardo Sanchez <edsanchez629@gmail.com>
2020-03-01 00:22:13 -0800

    adding files

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
+var pdf = require('html-pdf');
+ 
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptuser() {
+  inquirer.prompt({
+  message: "Enter your GitHub username:",
+  name: "username"
+  })
+};
